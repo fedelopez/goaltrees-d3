f7370ca658f02f55c04917ae3ddc7d05a38f99b7
Fede Lopez <fede.lopez@gmail.com>
2016-01-26 23:58:37 +1100

    Lay out boxes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,47 @@
 var runApp = function () {
-    d3.select("#app")
+
+    var boxW = 50;
+    var boxH = 50;
+    var maxBoxH = boxH * 2;
+
+    var baseLineW = boxW * boxes.length;
+    var baseLineH = 10;
+
+    var terrainW = baseLineW;
+    var terrainH = maxBoxH * 2 + baseLineH;
+
+    var container = d3.select("#app")
         .append("svg")
-        .attr("width", 50)
-        .attr("height", 50)
-        .append("circle")
-        .attr("cx", 25)
-        .attr("cy", 25)
-        .attr("r", 25)
-        .style("fill", "purple");
+        .attr("width", terrainW)
+        .attr("height", terrainH);
+
+    container.append("rect")
+        .attr("x", 0)
+        .attr("y", terrainH - baseLineH)
+        .attr("width", baseLineW)
+        .attr("height", baseLineH)
+        .style("fill", "lightgrey");
+
+    container.selectAll("rect")
+        .data(boxes)
+        .enter()
+        .append("rect")
+        .attr("x", function (box) {
+            return box.x * boxW;
+        })
+        .attr("y", function (box) {
+            return terrainH - baseLineH - (boxH * box.height) - (boxH * box.height * box.y);
+        })
+        .attr("width", boxW)
+        .attr("height", function (box) {
+            return box.height * boxH;
+        })
+        .style("stroke", "lightgrey")
+        .style("fill", "none")
+        .style("stroke-width", 1)
+        .style("fill", function (box) {
+            return box.color;
+        });
 };
 
 
---
+++ src/boxes.js
@@ -0,0 +1,59 @@
+var boxes = [
+    {},//todo, if we don't put dummy entry, the next one is not taken into account
+    {
+        "name": "B1",
+        "color": "#0000FF",
+        "x": 0,
+        "y": 1,
+        "height": 1
+    },
+    {
+        "name": "G1",
+        "color": "#008000",
+        "x": 0,
+        "y": 0,
+        "height": 1
+    },
+    {
+        "name": "R1",
+        "color": "#FF0000",
+        "x": 1,
+        "y": 0,
+        "height": 2
+    },
+    {
+        "name": "O1",
+        "color": "orange",
+        "x": 2,
+        "y": 0,
+        "height": 1
+    },
+    {
+        "name": "B2",
+        "color": "#0000FF",
+        "x": 3,
+        "y": 0,
+        "height": 2
+    },
+    {
+        "name": "G2",
+        "color": "#008000",
+        "x": 4,
+        "y": 0,
+        "height": 1
+    },
+    {
+        "name": "R2",
+        "color": "#FF0000",
+        "x": 5,
+        "y": 0,
+        "height": 1
+    },
+    {
+        "name": "O2",
+        "color": "orange",
+        "x": 6,
+        "y": 0,
+        "height": 2
+    }
+];

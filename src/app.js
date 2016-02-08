95be35576133146dcbbda969fece45b7dafa70d5
Fede Lopez <fede.lopez@gmail.com>
2016-02-08 23:01:23 +1100

    Wip box state

--- src/app.js
+++ src/app.js
@@ -1,20 +1,144 @@
-var boxW = 50;
-var boxH = 50;
-var maxBoxH = boxH * 2;
+const boxW = 50;
+const boxH = 50;
+const maxBoxH = boxH * 2;
 
-var baseLineW = boxW * 10;
-var baseLineH = 10;
+const xLimit = 10;
+const baseLineW = boxW * xLimit;
+const baseLineH = 10;
 
-var craneW = 10;
-var craneH = 40;
+const craneW = 10;
+const craneH = 40;
 
-var magnetHookW = 30;
-var magnetHookH = 15;
+const magnetHookW = 30;
+const magnetHookH = 15;
 
-var terrainW = baseLineW;
-var terrainH = maxBoxH * 3;
+const terrainW = baseLineW;
+const terrainH = maxBoxH * 3;
 
-var runApp = function () {
+
+var State = function (boxPile, srcBox, dstX, dstY) {
+
+    this.isGoal = function () {
+        function isBoxPresent(box) {
+            return box.x === dstX && box.y === dstY;
+        }
+
+        return !boxPile.hasBoxAbove(srcBox) && !boxPile.every(isBoxPresent)
+    };
+
+    this.getSrcBox = function () {
+        return srcBox;
+    };
+
+    this.getDstX = function () {
+        return dstX;
+    };
+
+    this.getDstY = function () {
+        return dstY;
+    };
+
+    this.getNeighbors = function () {
+        var neighbors = [];
+        if (boxPile.isBoxAbove(srcBox)) {
+            const topMostBox = boxPile.topmostBoxAbove(srcBox);
+            const point = boxPile.firstFreeCoordinate(topMostBox, dstX);
+            neighbors.push(new Action(topMostBox.name, point.x, point.y));
+        }
+        return neighbors;
+    };
+};
+
+var Action = function (boxName, x, y) {
+
+    this.getBoxName = function () {
+        return boxName;
+    };
+
+    this.getDstX = function () {
+        return x;
+    };
+
+    this.getDstY = function () {
+        return y;
+    };
+
+};
+
+var Box = function (name, color, x, y, height, width) {
+    this.name = name;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.width = width;
+
+    var self = this;
+
+    function onTopOf(candidateBox) {
+        return self.y < candidateBox.y && self.x === candidateBox.x
+    }
+
+    this.hasBoxAbove = function (allBoxes) {
+        return allBoxes.some(onTopOf)
+    };
+};
+
+var BoxPile = function (allBoxes, width, height) {
+    var self = this;
+
+    this.isBoxAbove = function (box) {
+        return allBoxes.some(function (candidateBox) {
+            return box.y < candidateBox.y && box.x === candidateBox.x
+        });
+    };
+
+    this.topmostBoxAbove = function (srcBox) {
+        var topMostBox = undefined;
+
+        allBoxes.filter(function (box) {
+            return box != srcBox && box.x === srcBox.x && box.y > srcBox.y;
+        }).forEach(function (box) {
+            console.log(box.name);
+            if (topMostBox === undefined || box.y > topMostBox.y) topMostBox = box;
+        });
+        return topMostBox;
+    };
+
+    this.firstFreeCoordinate = function (box, excludedAbscissa) {
+        for (var x = 0; x < this.getWidth(); x++) {
+            const emptySpot = allBoxes.every(function (currentBox) {
+                return currentBox.x != x;
+            });
+            if (emptySpot) {
+                return {"x": x, "y": 0};
+            } else {
+                const result = allBoxes.filter(function (currentBox) {
+                    return (!self.isBoxAbove(currentBox) && currentBox.x != box.x && currentBox.x != excludedAbscissa);
+                });
+                if (result.length > 0) return {"x": result[0].x, "y": result[0].y + 1}
+            }
+        }
+        return undefined;
+    };
+
+    this.getWidth = function () {
+        return width;
+    }
+};
+
+const boxes = [
+    new Box("B1", "#0000FF", 1, 1, 1, 1),
+    new Box("G1", "#008000", 1, 0, 1, 1),
+    new Box("R1", "#FF0000", 2, 0, 2, 1),
+    new Box("O1", "orange", 3, 0, 1, 1),
+    new Box("B2", "#0000FF", 4, 0, 2, 2),
+    new Box("G2", "#008000", 6, 0, 1, 2),
+    new Box("R2", "#FF0000", 8, 0, 1, 1),
+    new Box("O2", "orange", 9, 0, 2, 1)
+];
+
+function runApp() {
     d3.select("#app").selectAll("*").remove();
 
     var container = d3.select("#app")
@@ -83,9 +207,9 @@ var runApp = function () {
             console.log("Box " + box.name + " clicked.");
             dropCrane(box);
         });
-};
+}
 
-var dropCrane = function (targetBox) {
+function dropCrane(targetBox) {
     var boxElement = document.getElementById("box-" + targetBox.name);
     d3.selectAll("#crane")
         .transition()
@@ -117,13 +241,28 @@ var dropCrane = function (targetBox) {
         .each("end", function () {
             console.log("Magnet attached");
         });
-};
+}
 
-var moveBox = function (allBoxes, srcBox, destBox) {
-    if (!srcBox.hasBoxAbove(allBoxes) && !destBox.hasBoxAbove(allBoxes)) {
-        return [{box: srcBox, x: destBox.x, y: 1 + destBox.y + destBox.height}];
-    } else {
-        return null;
-    }
-};
+function moveBox(initialState) {
+
+    var doMoveBox = function (visited, frontier) {
+        if (frontier.length == 0) throw "No solution";
+        else {
+            var path = frontier[0];
+            var state = path[0];
+            if (state.isGoal()) {
+                var x = state.getDstBox().x;
+                var y = 1 + state.getDstBox().y + state.getDstBox().height;
+                var boxName = state.getSrcBox().name;
+                return [new Action(boxName, x, y)];
+            } else {
+                var neighbors = [];
+                return null;
+            }
+
+        }
+    };
+    return doMoveBox([], [[initialState]]);
+}
+
 
--- test/appSpec.js
+++ test/appSpec.js
@@ -1,6 +1,6 @@
 describe("goaltrees-d3", function () {
 
-    var allBoxes;
+    var allBoxes, boxPile;
 
     beforeEach(function () {
         allBoxes = [
@@ -13,6 +13,7 @@ describe("goaltrees-d3", function () {
             new Box("R2", "red", 8, 0, 1, 1),
             new Box("O2", "orange", 9, 0, 2, 1)
         ];
+        boxPile = new BoxPile(allBoxes, 10, 3);
     });
 
     describe("move suite", function () {
@@ -20,8 +21,12 @@ describe("goaltrees-d3", function () {
         it("should return one operation when both boxes have no other box on top", function () {
             var boxR1 = allBoxes[2];
             var boxR2 = allBoxes[6];
-            var moves = moveBox(allBoxes, boxR2, boxR1);
-            expect(moves).toEqual([{box: boxR2, x: 2, y: 3}]);
+            var state = new State(allBoxes, boxR2, boxR1);
+            var moves = moveBox(state);
+            expect(moves.length).toBe(1);
+            expect(moves[0].getBoxName()).toBe(boxR2.name);
+            expect(moves[0].getDstX()).toBe(2);
+            expect(moves[0].getDstY()).toBe(3);
         });
 
         /**
@@ -30,24 +35,55 @@ describe("goaltrees-d3", function () {
         it("should clear the top of the destination box", function () {
             var boxG1 = allBoxes[1];
             var boxO1 = allBoxes[3];
-            var moves = moveBox(allBoxes, boxO1, boxG1);
-            expect(moves).toEqual([
-                {box: allBoxes[0], x: 0, y: 0},
-                {box: boxO1, x: 1, y: 1}
-            ]);
+            var moves = moveBox(new State(allBoxes, boxO1, boxG1));
+            expect(moves.length).toBe(2);
+
+            expect(moves[0].getBoxName()).toBe(allBoxes[0].name);
+            expect(moves[0].getDstX()).toBe(0);
+            expect(moves[0].getDstY()).toBe(0);
+
+            expect(moves[1].getBoxName()).toBe(boxO1.name);
+            expect(moves[1].getDstX()).toBe(1);
+            expect(moves[1].getDstY()).toBe(1);
+
         });
 
     });
 
-    describe("has box above suite", function () {
+    describe("state suite", function () {
+
+        it("should propose getting rid of the box of the top to get it out of the way", function () {
+            var g1 = allBoxes[1];
+            var r1 = allBoxes[2];
+            var neighbors = new State(boxPile, g1, r1.x, r1.y + 1).getNeighbors();
+            expect(neighbors.length).toBe(1);
+
+            expect(neighbors[0].getBoxName()).toBe("B1");
+            expect(neighbors[0].getDstX()).toBe(0);
+            expect(neighbors[0].getDstY()).toBe(0);
+        });
+
+        //todo make another test where it proposes a place on top of a box
+
+    });
+
+    describe("box pile suite", function () {
 
         it("should return false when a box has no other box above", function () {
-            expect(allBoxes[0].hasBoxAbove(allBoxes)).toBeFalsy();
-            expect(allBoxes[2].hasBoxAbove(allBoxes)).toBeFalsy();
+            expect(boxPile.isBoxAbove(allBoxes[0])).toBeFalsy();
+            expect(boxPile.isBoxAbove(allBoxes[2])).toBeFalsy();
         });
 
         it("should return true when a box has another box above", function () {
-            expect(allBoxes[1].hasBoxAbove(allBoxes)).toBeTruthy();
+            expect(boxPile.isBoxAbove(allBoxes[1])).toBeTruthy();
+        });
+
+        it("should return undefined when a box no topmost box above", function () {
+            expect(boxPile.topmostBoxAbove(allBoxes[0])).toBeUndefined();
+        });
+
+        it("should return the topmost box above a given box", function () {
+            expect(boxPile.topmostBoxAbove(allBoxes[1])).toBe(allBoxes[0]);
         });
 
     });
